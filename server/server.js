0cec669569e550aa4f4ca0573e578bd1b1eeca5d
tomson <rtomson687@gmail.com>
2024-08-24 10:58:21 +0530

    file change

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const ACTIONS = require('./src/Actions');
+const ACTIONS = require('../src/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
